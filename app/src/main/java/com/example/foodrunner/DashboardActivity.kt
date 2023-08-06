d53a430dcf9ae4b496cd416fe99959fea7cb77f2
xauravww <sauravmaheshwari8@gmail.com>
2023-08-06 16:39:01 +0530

    added toolbar and drawerlayout navigationview

--- app/src/main/java/com/example/foodrunner/DashboardActivity.kt
+++ app/src/main/java/com/example/foodrunner/DashboardActivity.kt
@@ -4,20 +4,50 @@ import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class DashboardActivity : AppCompatActivity() {
     lateinit var txtDashboard: TextView
-    lateinit var sharedPreferences:SharedPreferences
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinatorLayout: CoordinatorLayout
+    lateinit var toolbar: Toolbar
+    lateinit var frameLayout: FrameLayout
+    lateinit var navigationView: NavigationView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-        sharedPreferences = getSharedPreferences(getString(R.string.shared_preferences),
+        sharedPreferences = getSharedPreferences(
+            getString(R.string.shared_preferences),
             MODE_PRIVATE
         )
         txtDashboard = findViewById(R.id.txtDashboard)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        frameLayout = findViewById(R.id.frameLayout)
+        navigationView = findViewById(R.id.navigationView)
+        setUpToolbar()
+
+        val actionBarDrawertoggle = ActionBarDrawerToggle(
+            this@DashboardActivity,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(actionBarDrawertoggle)
+        actionBarDrawertoggle.syncState()
+
         if (intent != null) {
             val details = intent.getBundleExtra("details")
             val data = details?.getString("data")
@@ -42,19 +72,19 @@ class DashboardActivity : AppCompatActivity() {
 
                 "login" -> {
                     val text =
-                        "Mobile ${sharedPreferences.getString("mobile","USER-Anonymous")} " +
+                        "Mobile ${sharedPreferences.getString("mobile", "USER-Anonymous")} " +
                                 "Welcome to our app"
                     txtDashboard.text = text
                 }
 
                 else -> {
-                    val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn",false)
-                    if(isLoggedIn){
+                    val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+                    if (isLoggedIn) {
                         val text =
-                            "Mobile ${sharedPreferences.getString("mobile","USER-Anonymous")} " +
+                            "Mobile ${sharedPreferences.getString("mobile", "USER-Anonymous")} " +
                                     "Welcome to our app"
                         txtDashboard.text = text
-                    }else{
+                    } else {
                         val text = "No data entered"
                         txtDashboard.text = text
                     }
@@ -63,5 +93,20 @@ class DashboardActivity : AppCompatActivity() {
 
 
         }
+    }
+
+    private fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Toolbar Title"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
